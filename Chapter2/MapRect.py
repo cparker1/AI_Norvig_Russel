df1a6d59c0fa12abd07d79293720d653720bda8c
Charles Parker <charles.a.parker@seagate.com>
2014-11-05 10:27:39 -0700

    Added in matplotlib graphing and a random state setter. Still not happy with the map implementation.  There are plenty of #TODOs to resolve.

--- Chapter2/MapRect.py
+++ Chapter2/MapRect.py
@@ -1,15 +1,20 @@
-# import matplotlib.pyplot as plt
-# import numpy as np
-__author__ =   "Charles A. Parker"
+import matplotlib.pyplot as plt
+import numpy as np
+__author__ = "Charles A. Parker"
 
 # TODO use logging module
 # TODO figure out wtf i'm doing
 
 
+def get_rand_weighted_val(values, probabilities):
+    bins = np.add.accumulate(probabilities)
+    return values[np.digitize(np.random.random_sample(1), bins)]
+
+
 # TODO do I really need this class?...
 class MapCell(object):
     """
-    MapCell is a single element of a map.  It is intended to be 
+    MapCell is a single element of a map.  It is intended to be
     built up into a larger map.
     """
     def __init__(self):
@@ -17,7 +22,7 @@ class MapCell(object):
         pass
 
     def set_state(self, value):
-        if value in [0,1]:
+        if value in [0, 1]:
             self.state = value
 
     def get_state(self):
@@ -45,7 +50,8 @@ class MapRect(object):
     def set_dimensions(self, col_len, row_len):
         self.x_dim = col_len
         self.row_len = row_len
-        self.array = [[self.cell_type() for x in range(col_len)] for y in range(row_len)]
+        self.array = [[self.cell_type() for x in range(col_len)]
+                      for y in range(row_len)]
         pass
 
     def get_dimensions(self):
@@ -56,6 +62,10 @@ class MapRect(object):
             return self.array[row][col]
         except:
             return None
+
+    def apply_function_to_map(self, map_function):
+        if map_function is not None:
+            self.array = [map(map_function, row) for row in self.array]
 
     # TODO Probably a more concise way to do this, too
     def get_next_neighbor(self, cur_row, cur_col, direction):
@@ -83,38 +93,18 @@ class MapRect(object):
 
     # TODO Figure out how to do this correctly.  Trying
     # to apply an arbitrary functionto the whole data set
-    def print_map(self, map_function = None):
-        for row in self.array:
-            if map_function is None:
-                print row
-            else:
-                print map(map_function, row)
-
+    def display_map(self, map_function=None):
+        grid = [map(map_function, row) for row in self.array]
+        plt.imshow(grid)
+        plt.show()
 
 
 if __name__ == "__main__":
     test = MapRect(MapCell)
-    test.set_dimensions(10,5)
+    test.set_dimensions(10, 5)
     test.get_element(0, 0).toggle_state()
 
-    test.print_map(MapCell.get_state)
+    for x in [ele for row in test.array for ele in row]:
+        x.set_state(get_rand_weighted_val([0, 1], [0.3, 0.7]))
 
-    print "\n{}".format(test.get_next_neighbor(4,4,"DOWN"))
-
-# EXAMPLE PLOTTING STUFF
-#     methods = [None, 'none', 'nearest', 'bilinear', 'bicubic', 'spline16',
-#                      'spline36', 'hanning', 'hamming', 'hermite', 'kaiser', 'quadric',
-#                      'catrom', 'gaussian', 'bessel', 'mitchell', 'sinc', 'lanczos']
-
-#     grid = np.random.rand(4, 4)
-
-#     fig, axes = plt.subplots(3, 6, figsize=(12, 6),
-#                                                      subplot_kw={'xticks': [], 'yticks': []})
-
-#     fig.subplots_adjust(hspace=0.3, wspace=0.05)
-
-#     for ax, interp_method in zip(axes.flat, methods):
-#             ax.imshow(grid, interpolation=interp_method)
-#             ax.set_title(interp_method)
-
-#     plt.show()
+    test.display_map(MapCell.get_state)
